47f29737fcfe515de4f357146e5fe216c6c77f6e
Ravindra Singh Panwar <rajsinghravi25.87@gmail.com>
2015-07-29 08:29:51 +0530

    Date picker plugin and js

---
+++ assets/js/proto.js
@@ -0,0 +1,30 @@
+function CustomJS() {
+    this.URL =  "http://www.google.co.in";
+}
+
+CustomJS.prototype = {
+    pageLoadInitializations: function() {
+        $('.date-picker').datepicker({
+            autoclose: true,
+            format:'dd-mm-yyyy'
+        });
+    },
+    eventInitializations: function() {
+        var self = this;
+        console.log(self.URL);
+        $(".get-content").on('click',function() {
+            $.ajax({
+                url: "ajax.php",
+                type: "GET",
+                dataType: "JSON",
+                data : {
+                    user_age : $("#user_age").val()
+                },
+                success: function (data) {
+                    $("#hello").html("Hello "+data.name +" your age is : "+data.age +" and server has a msg for you : "+data.msg);
+                }
+            });
+        });
+    }
+}
+var object = new CustomJS();
